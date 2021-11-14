a71143a3ad17efa0d94c9062dac9ee654701e679
nicozucco <nicozucco_5@hotmail.com>
2021-11-14 20:36:38 -0300

    Entrega 7

--- js/cart.js
+++ js/cart.js
@@ -1,4 +1,7 @@
 var CART_URL = "https://japdevdep.github.io/ecommerce-api/cart/654.json"
+let porcentajeenvio = 0.15;
+
+
 
 function showCart(array) {
 
@@ -17,17 +20,17 @@ function showCart(array) {
         let subs = (precio * articles.count)
 
         contenido += `
-    <div class="row">
+    <div class="row" id="producto_${i}">
       <div class="col-2 border"><img src='${articles.src}' width=100px></div>
-      <div class="col-3 border">${articles.name}</div>
-      <div class="col-2 border">${articles.currency} ${articles.unitCost}</div>
-      <div class="col-2 border">
+      <div class="col-3 border pt-2">${articles.name}</div>
+      <div class="col-2 border pt-2">${articles.currency} ${articles.unitCost}</div>
+      <div class="col-2 border pt-2">
       <input style="width:60px;" onchange="calcSubtotal(${precio}, ${i})" type="number" min="1" id="cantidad${i}" value="${articles.count}">
       </div>
-      <div class="col-2 border">
+      <div class="col-2 border pt-2">
       <span class="subtotal" id="prodSubtotal${i}">${subs}</span>
       </div>
-      <div class="col-1 border"><button type="button" class="btn btn-danger">Eliminar</button></div>
+      <div class="col-1 border text-center pt-2"><button type="button" class="btn btn-danger center">X</button></div>
     </div>
 `
         document.getElementById("carrito").innerHTML = contenido;
@@ -43,6 +46,7 @@ function calcSubtotal(costo, i) {
     document.getElementById(`prodSubtotal${i}`).innerHTML = subtotal;
 
     calcTotal();
+    calcEnvio();
 };
 
 function calcTotal() {
@@ -55,8 +59,38 @@ function calcTotal() {
     }
 
     document.getElementById("total").innerHTML = total;
+    document.getElementById("totalsinenvio").innerHTML = total;
 };
 
+// Calcular el porcentaje del envio
+function cambiarporcentaje(porcentaje){
+  porcentajeenvio = porcentaje;
+  calcEnvio();
+};
+
+// Calcula el envio y el total
+function calcEnvio(){
+  let totalsinenvio = document.getElementById("totalsinenvio").innerHTML
+  let costoenvio = Math.round(parseInt(totalsinenvio * porcentajeenvio));
+  document.getElementById("costoenvio").innerHTML = costoenvio;
+  document.getElementById("totalconenvio").innerHTML = Math.round(parseInt(totalsinenvio) + costoenvio);
+};
+
+// Seleccionar el método de pago
+function paymethod(){
+  card = document.getElementById("validationPay01");
+  bank = document.getElementById("validationPay02");
+
+  if(card.checked){
+    document.getElementById("cardnumber").removeAttribute("disabled");
+    document.getElementById("cardvalid").removeAttribute("disabled");
+    document.getElementById("bankaccount").setAttribute("disabled", "disabled");
+  }else if (bank.checked){
+    document.getElementById("cardnumber").setAttribute("disabled", "disabled");
+    document.getElementById("cardvalid").setAttribute("disabled", "disabled");
+    document.getElementById("bankaccount").removeAttribute("disabled");
+  }
+}
 
 document.addEventListener("DOMContentLoaded", function (e) {
 
@@ -65,7 +99,24 @@ document.addEventListener("DOMContentLoaded", function (e) {
             cart = resultObj.data.articles;
 
             showCart(cart);
+            calcEnvio();
         }
     })
-
 });
+
+// Validación de Bootstrap
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+      var forms = document.getElementsByClassName('needs-validation');
+      var validation = Array.prototype.filter.call(forms, function(form) {
+        form.addEventListener('submit', function(event) {
+          if (form.checkValidity() === false) {
+            event.preventDefault();
+            event.stopPropagation();
+          }
+          form.classList.add('was-validated');
+        }, false);
+      });
+    }, false);
+  })();
